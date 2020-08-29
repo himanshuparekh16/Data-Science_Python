aa680238e8b61cbbf9b48faea5581938ee79a0a3
himanshuparekh16 <70227082+himanshuparekh16@users.noreply.github.com>
2020-08-29 23:45:19 +0530

    Add files via upload

---
+++ dataset_salary_exercise.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[5]:
+
+
+data = pd.read_csv("http://rcs.bu.edu/examples/python/data_analysis/Salaries.csv")
+
+
+# In[6]:
+
+
+data
+
+
+# In[8]:
+
+
+len(data)
+
+
+# In[12]:
+
+
+len(data.columns)
+
+
+# In[13]:
+
+
+data.columns
+
+
+# In[15]:
+
+
+data.dtypes
+
+
+# In[ ]:
+
+
+
+
