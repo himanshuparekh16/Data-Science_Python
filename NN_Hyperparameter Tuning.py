f810996b2b0344a0acf73e2a8f0eea7ad452a83f
himanshuparekh16 <70227082+himanshuparekh16@users.noreply.github.com>
2021-02-28 21:55:39 +0530

    Add files via upload

---
+++ NN_Hyperparameter Tuning.py
@@ -0,0 +1,344 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Importing the necessary packages
+import pandas as pd
+import numpy as np
+import keras
+from sklearn.preprocessing import StandardScaler
+
+
+# In[2]:
+
+
+# load pima indians dataset
+dataset = np.loadtxt("pima-indians-diabetes.csv", delimiter=",")
+
+
+# In[3]:
+
+
+X = dataset[:,0:8]
+y = dataset[:,8]
+
+
+# In[4]:
+
+
+# Standardization
+a = StandardScaler()
+a.fit(X)
+X_standardized = a.transform(X)
+
+
+# In[5]:
+
+
+pd.DataFrame(X_standardized).describe()
+
+
+# #### Tuning of Hyperparameters :- Batch Size and Epochs
+
+# In[6]:
+
+
+# Importing the necessary packages
+from sklearn.model_selection import GridSearchCV, KFold
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.optimizers import Adam
+
+
+# In[7]:
+
+
+# create model
+def create_model():
+    model = Sequential()
+    model.add(Dense(12, input_dim=8, init='uniform', activation='relu'))
+    model.add(Dense(8, init='uniform', activation='relu'))
+    model.add(Dense(1, init='uniform', activation='sigmoid'))
+    
+    adam=Adam(lr=0.01)
+    model.compile(loss='binary_crossentropy', optimizer=adam, metrics=['accuracy'])
+    return model
+
+
+# In[ ]:
+
+
+# Create the model
+model = KerasClassifier(build_fn = create_model,verbose = 0)
+# Define the grid search parameters
+batch_size = [10,20,40]
+epochs = [10,150,200]
+# Make a dictionary of the grid search parameters
+param_grid = dict(batch_size = batch_size,epochs = epochs)
+# Build and fit the GridSearchCV
+grid = GridSearchCV(estimator = model,param_grid = param_grid,cv = KFold(),verbose = 10)
+grid_result = grid.fit(X_standardized,y)
+
+
+# In[ ]:
+
+
+# Summarize the results
+print('Best : {}, using {}'.format(grid_result.best_score_,grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+  print('{},{} with: {}'.format(mean, stdev, param))
+
+
+# #### Tuning of Hyperparameters:- Learning rate and Drop out rate
+
+# In[ ]:
+
+
+from keras.layers import Dropout
+
+# Defining the model
+
+def create_model(learning_rate,dropout_rate):
+    model = Sequential()
+    model.add(Dense(8,input_dim = 8,kernel_initializer = 'normal',activation = 'relu'))
+    model.add(Dropout(dropout_rate))
+    model.add(Dense(4,input_dim = 8,kernel_initializer = 'normal',activation = 'relu'))
+    model.add(Dropout(dropout_rate))
+    model.add(Dense(1,activation = 'sigmoid'))
+    
+    adam = Adam(lr = learning_rate)
+    model.compile(loss = 'binary_crossentropy',optimizer = adam,metrics = ['accuracy'])
+    return model
+
+# Create the model
+
+model = KerasClassifier(build_fn = create_model,verbose = 0,batch_size = 40,epochs = 10)
+
+# Define the grid search parameters
+
+learning_rate = [0.001,0.01,0.1]
+dropout_rate = [0.0,0.1,0.2]
+
+# Make a dictionary of the grid search parameters
+
+param_grids = dict(learning_rate = learning_rate,dropout_rate = dropout_rate)
+
+# Build and fit the GridSearchCV
+
+grid = GridSearchCV(estimator = model,param_grid = param_grids,cv = KFold(),verbose = 10)
+grid_result = grid.fit(X_standardized,y)
+
+
+# In[ ]:
+
+
+# Summarize the results
+print('Best : {}, using {}'.format(grid_result.best_score_,grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+  print('{},{} with: {}'.format(mean, stdev, param))
+
+
+# #### Tuning of Hyperparameters:- Activation Function and Kernel Initializer
+
+# In[ ]:
+
+
+# Defining the model
+
+def create_model(activation_function,init):
+    model = Sequential()
+    model.add(Dense(8,input_dim = 8,kernel_initializer = init,activation = activation_function))
+    model.add(Dropout(0.1))
+    model.add(Dense(4,input_dim = 8,kernel_initializer = init,activation = activation_function))
+    model.add(Dropout(0.1))
+    model.add(Dense(1,activation = 'sigmoid'))
+    
+    adam = Adam(lr = 0.001)
+    model.compile(loss = 'binary_crossentropy',optimizer = adam,metrics = ['accuracy'])
+    return model
+
+# Create the model
+
+model = KerasClassifier(build_fn = create_model,verbose = 0,batch_size = 40,epochs = 10)
+
+# Define the grid search parameters
+activation_function = ['softmax','relu','tanh','linear']
+init = ['uniform','normal','zero']
+
+# Make a dictionary of the grid search parameters
+param_grids = dict(activation_function = activation_function,init = init)
+
+# Build and fit the GridSearchCV
+
+grid = GridSearchCV(estimator = model,param_grid = param_grids,cv = KFold(),verbose = 10)
+grid_result = grid.fit(X_standardized,y)
+
+
+# In[ ]:
+
+
+# Summarize the results
+print('Best : {}, using {}'.format(grid_result.best_score_,grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+  print('{},{} with: {}'.format(mean, stdev, param))
+
+
+# #### Tuning of Hyperparameter :-Number of Neurons in activation layer
+
+# In[ ]:
+
+
+# Defining the model
+
+def create_model(neuron1,neuron2):
+    model = Sequential()
+    model.add(Dense(neuron1,input_dim = 8,kernel_initializer = 'uniform',activation = 'tanh'))
+    model.add(Dropout(0.1))
+    model.add(Dense(neuron2,input_dim = neuron1,kernel_initializer = 'uniform',activation = 'tanh'))
+    model.add(Dropout(0.1))
+    model.add(Dense(1,activation = 'sigmoid'))
+    
+    adam = Adam(lr = 0.001)
+    model.compile(loss = 'binary_crossentropy',optimizer = adam,metrics = ['accuracy'])
+    return model
+
+# Create the model
+
+model = KerasClassifier(build_fn = create_model,verbose = 0,batch_size = 40,epochs = 10)
+
+# Define the grid search parameters
+
+neuron1 = [4,8,16]
+neuron2 = [2,4,8]
+
+# Make a dictionary of the grid search parameters
+
+param_grids = dict(neuron1 = neuron1,neuron2 = neuron2)
+
+# Build and fit the GridSearchCV
+
+grid = GridSearchCV(estimator = model,param_grid = param_grids,cv = KFold(),verbose = 10)
+grid_result = grid.fit(X_standardized,y)
+
+
+# In[ ]:
+
+
+# Summarize the results
+print('Best : {}, using {}'.format(grid_result.best_score_,grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+  print('{},{} with: {}'.format(mean, stdev, param))
+
+
+# #### Training model with optimum values of Hyperparameters
+
+# In[ ]:
+
+
+from sklearn.metrics import classification_report, accuracy_score
+
+# Defining the model
+
+def create_model():
+    model = Sequential()
+    model.add(Dense(16,input_dim = 8,kernel_initializer = 'uniform',activation = 'tanh'))
+    model.add(Dropout(0.1))
+    model.add(Dense(4,input_dim = 16,kernel_initializer = 'uniform',activation = 'tanh'))
+    model.add(Dropout(0.1))
+    model.add(Dense(1,activation = 'sigmoid'))
+    
+    adam = Adam(lr = 0.001) #sgd = SGD(lr=learning_rate, momentum=momentum, decay=decay_rate, nesterov=False)
+    model.compile(loss = 'binary_crossentropy',optimizer = adam,metrics = ['accuracy'])
+    return model
+
+# Create the model
+
+model = KerasClassifier(build_fn = create_model,verbose = 0,batch_size = 40,epochs = 10)
+
+# Fitting the model
+
+model.fit(X_standardized,y)
+
+# Predicting using trained model
+
+y_predict = model.predict(X_standardized)
+
+# Printing the metrics
+print(accuracy_score(y,y_predict))
+
+
+# # Hyperparameters all at once
+
+# 
+# The hyperparameter optimization was carried out by taking 2 hyperparameters at once. We may have missed the best values. The performance can be further improved by finding the optimum values of hyperparameters all at once given by the code snippet below.
+# #### This process is computationally expensive.
+
+# In[ ]:
+
+
+def create_model(learning_rate,dropout_rate,activation_function,init,neuron1,neuron2):
+    model = Sequential()
+    model.add(Dense(neuron1,input_dim = 8,kernel_initializer = init,activation = activation_function))
+    model.add(Dropout(dropout_rate))
+    model.add(Dense(neuron2,input_dim = neuron1,kernel_initializer = init,activation = activation_function))
+    model.add(Dropout(dropout_rate))
+    model.add(Dense(1,activation = 'sigmoid'))
+    
+    adam = Adam(lr = learning_rate)
+    model.compile(loss = 'binary_crossentropy',optimizer = adam,metrics = ['accuracy'])
+    return model
+
+# Create the model
+
+model = KerasClassifier(build_fn = create_model,verbose = 0)
+
+# Define the grid search parameters
+
+batch_size = [10,20,40]
+epochs = [10,50,100]
+learning_rate = [0.001,0.01,0.1]
+dropout_rate = [0.0,0.1,0.2]
+activation_function = ['softmax','relu','tanh','linear']
+init = ['uniform','normal','zero']
+neuron1 = [4,8,16]
+neuron2 = [2,4,8]
+
+# Make a dictionary of the grid search parameters
+
+param_grids = dict(batch_size = batch_size,epochs = epochs,learning_rate = learning_rate,dropout_rate = dropout_rate,
+                   activation_function = activation_function,init = init,neuron1 = neuron1,neuron2 = neuron2)
+
+# Build and fit the GridSearchCV
+
+grid = GridSearchCV(estimator = model,param_grid = param_grids,cv = KFold(),verbose = 10)
+grid_result = grid.fit(X_standardized,y)
+
+# Summarize the results
+print('Best : {}, using {}'.format(grid_result.best_score_,grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+  print('{},{} with: {}'.format(mean, stdev, param))
+
+
+# In[ ]:
+
+
+
+
